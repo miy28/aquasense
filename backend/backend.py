cc5fdad6f315764ac3eb27d4f2c22d2ac6c30853
miy28 <91354024+miy28@users.noreply.github.com>
2025-04-28 20:52:08 -0500

    pickle

--- backend/backend.py
+++ backend/backend.py
@@ -5,10 +5,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import datetime
 import json
+import pickle
+import os
 from ml.anomaly import detect_anomalies
 
-
 data = pd.DataFrame(columns=["sensor_type", "value", "timestamp"])
+
+if os.path.exists('data.pkl'):
+    with open('data.pkl', 'rb') as f:
+        data = pickle.load(f)
 
 def push_data(json_data):
     global data
@@ -24,6 +29,9 @@ def push_data(json_data):
         data = pd.concat([data, df_new_row], ignore_index=True)
         print("Added data.")
         print(data)
+
+        with open('data.pkl', 'wb') as f:
+                pickle.dump(data, f)
     
     except Exception as e:
         print("Error parsing JSON:", str(e))
